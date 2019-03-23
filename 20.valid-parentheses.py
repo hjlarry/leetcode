ebe269bf64453f4516df4b7c5c2b7c054bac56fe
he jiale <hjlarry@163.com>
2019-03-23 20:54:36 +0800

    add valid-partheses my solution

---
+++ 20.valid-parentheses.py
@@ -0,0 +1,78 @@
+#
+# @lc app=leetcode id=20 lang=python3
+#
+# [20] Valid Parentheses
+#
+# https://leetcode.com/problems/valid-parentheses/description/
+#
+# algorithms
+# Easy (35.82%)
+# Total Accepted:    538.7K
+# Total Submissions: 1.5M
+# Testcase Example:  '"()"'
+#
+# Given a string containing just the characters '(', ')', '{', '}', '[' and
+# ']', determine if the input string is valid.
+#
+# An input string is valid if:
+#
+#
+# Open brackets must be closed by the same type of brackets.
+# Open brackets must be closed in the correct order.
+#
+#
+# Note that an empty string is also considered valid.
+#
+# Example 1:
+#
+#
+# Input: "()"
+# Output: true
+#
+#
+# Example 2:
+#
+#
+# Input: "()[]{}"
+# Output: true
+#
+#
+# Example 3:
+#
+#
+# Input: "(]"
+# Output: false
+#
+#
+# Example 4:
+#
+#
+# Input: "([)]"
+# Output: false
+#
+#
+# Example 5:
+#
+#
+# Input: "{[]}"
+# Output: true
+#
+#
+#
+class Solution:
+    def isValid(self, s: str) -> bool:
+        result = []
+        for item in s:
+            if item in "([{":
+                result.append(item)
+            elif item is ")" and result and result[-1] is "(":
+                result.pop()
+            elif item is "]" and result and result[-1] is "[":
+                result.pop()
+            elif item is "}" and result and result[-1] is "{":
+                result.pop()
+            else:
+                return False
+        if result:
+            return False
+        return True
