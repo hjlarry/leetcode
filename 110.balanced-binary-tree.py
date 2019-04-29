f005aa4f2978b12aed1719da4627134e1e4f597a
he jiale <hjlarry@163.com>
2019-04-29 17:26:56 +0800

    add other solution of balanced-binary-tree

--- 110.balanced-binary-tree.py
+++ 110.balanced-binary-tree.py
@@ -12,23 +12,34 @@
 
 
 class Solution:
-    # 递归求出树高，再递归求出是否平衡二叉树，效率很低
-    def treeHeight(self, root):
-        if root is None:
-            return 0
-        root_l = self.treeHeight(root.left)
-        root_r = self.treeHeight(root.right)
-        if root_l > root_r:
-            return root_l + 1
-        return root_r + 1
+    # 递归求出树高，再递归求出是否平衡二叉树，效率很低， 19%
+    # def treeHeight(self, root):
+    #     if root is None:
+    #         return 0
+    #     root_l = self.treeHeight(root.left)
+    #     root_r = self.treeHeight(root.right)
+    #     if root_l > root_r:
+    #         return root_l + 1
+    #     return root_r + 1
 
+    # def isBalanced(self, root: TreeNode) -> bool:
+    #     if root is None:
+    #         return True
+    #     left_height = self.treeHeight(root.left)
+    #     right_height = self.treeHeight(root.right)
+    #     gap = right_height - left_height
+    #     if gap > 1 or gap < -1:
+    #         return False
+    #     return self.isBalanced(root.left) and self.isBalanced(root.right)
+    # 效率40.67%
     def isBalanced(self, root: TreeNode) -> bool:
-        if root is None:
-            return True
-        left_height = self.treeHeight(root.left)
-        right_height = self.treeHeight(root.right)
-        gap = right_height - left_height
-        if gap > 1 or gap < -1:
-            return False
-        return self.isBalanced(root.left) and self.isBalanced(root.right)
+        def check(root):
+            if root is None:
+                return 0
+            left = check(root.left)
+            right = check(root.right)
+            if left == -1 or right == -1 or abs(left - right) > 1:
+                return -1
+            return 1 + max(left, right)
 
+        return check(root) != -1
