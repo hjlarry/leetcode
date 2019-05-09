9c869e6c765a28f48e461e9280408b0459f2761b
hjlarry <hjlarry@163.com>
2019-05-09 13:57:13 +0800

    add palindrome top voted solution

--- 125.valid-palindrome.py
+++ 125.valid-palindrome.py
@@ -3,7 +3,6 @@
 #
 # [125] Valid Palindrome
 #
-import string
 
 
 class Solution:
@@ -18,17 +17,31 @@ class Solution:
     #         return True
     #     return False
 
-    # my better solution, 34.46%
+    # my better solution, 76.94 %
+    # def isPalindrome(self, s: str) -> bool:
+    #     test = [None]
+    #     for a in s:
+    #         if a.isalnum():
+    #             test.append(a.lower())
+    #     i = 1
+    #     while i < len(test) // 2 + 1:
+    #         if test[i] != test[-i]:
+    #             return False
+    #         i += 1
+    #     return True
+
+    # top voted solution, 46 %
     def isPalindrome(self, s: str) -> bool:
-        test = [None]
-        for a in s:
-            if a in string.ascii_letters + string.digits:
-                test.append(a.lower())
-        i = 1
-        while i < len(test) // 2 + 1:
-            if test[i] != test[-i]:
+        l, r = 0, len(s) - 1
+        while l < r:
+            while l < r and not s[l].isalnum():
+                l += 1
+            while l < r and not s[r].isalnum():
+                r -= 1
+            if s[l].lower() != s[r].lower():
                 return False
-            i += 1
+            l += 1
+            r -= 1
         return True
 
 
