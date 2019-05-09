0a88286804c9897176aa0e16c936cb982e9f0c7c
hjlarry <hjlarry@163.com>
2019-05-09 13:40:14 +0800

    add valid palindrome better solution

--- 125.valid-palindrome.py
+++ 125.valid-palindrome.py
@@ -7,16 +7,29 @@ import string
 
 
 class Solution:
+    # a slow solution, 13.8%
+    # def isPalindrome(self, s: str) -> bool:
+    #     test = []
+    #     for a in s:
+    #         if a in string.ascii_letters + string.digits:
+    #             test.append(a.lower())
+    #     test1 = list(reversed(test))
+    #     if test1 == test:
+    #         return True
+    #     return False
+
+    # my better solution, 34.46%
     def isPalindrome(self, s: str) -> bool:
-        # a slow solution, 13.8%
-        test = []
+        test = [None]
         for a in s:
             if a in string.ascii_letters + string.digits:
                 test.append(a.lower())
-        test1 = list(reversed(test))
-        if test1 == test:
-            return True
-        return False
+        i = 1
+        while i < len(test) // 2 + 1:
+            if test[i] != test[-i]:
+                return False
+            i += 1
+        return True
 
 
 # print(Solution().isPalindrome("A man, a plan, a canal: Panama"))
