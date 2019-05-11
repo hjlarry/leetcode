d7b7ab3c1a17419463ea83f6b62b04ff981848d2
he jiale <hjlarry@163.com>
2019-05-11 21:34:28 +0800

    add top voted solution for single number

--- 136.single-number.py
+++ 136.single-number.py
@@ -5,13 +5,22 @@
 #
 class Solution:
     # my solution, only 5.73%
+    # def singleNumber(self, nums) -> int:
+    #     current = nums.pop()
+    #     while True:
+    #         if current in nums:
+    #             nums.remove(current)
+    #             current = nums.pop()
+    #         else:
+    #             break
+    #     return current
+    # top voted solution , 45.21%
+    # def singleNumber(self, nums) -> int:
+    #     res = 0
+    #     for num in nums:
+    #         res ^= num
+    #     return res
+    # top voted solution , 94.86%
     def singleNumber(self, nums) -> int:
-        current = nums.pop()
-        while True:
-            if current in nums:
-                nums.remove(current)
-                current = nums.pop()
-            else:
-                break
-        return current
+        return 2 * sum(set(nums)) - sum(nums)
 
