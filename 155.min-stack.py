7a824866f8866d24bd3a12ce182a81bafa82fbf3
hjlarry <hjlarry@163.com>
2019-05-20 17:47:37 +0800

    add min-stack top voted solution

--- 155.min-stack.py
+++ 155.min-stack.py
@@ -5,6 +5,39 @@
 #
 
 # my solution 13.8 %
+# class MinStack(object):
+#     def __init__(self):
+#         """
+#         initialize your data structure here.
+#         """
+#         self.stack = []
+
+#     def push(self, x):
+#         """
+#         :type x: int
+#         :rtype: None
+#         """
+#         self.stack.append(x)
+
+#     def pop(self):
+#         """
+#         :rtype: None
+#         """
+#         self.stack.pop()
+
+#     def top(self):
+#         """
+#         :rtype: int
+#         """
+#         return self.stack[-1]
+
+#     def getMin(self):
+#         """
+#         :rtype: int
+#         """
+#         return min(self.stack)
+
+# top voted solution 45.69%
 class MinStack(object):
     def __init__(self):
         """
@@ -17,7 +50,10 @@ class MinStack(object):
         :type x: int
         :rtype: None
         """
-        self.stack.append(x)
+        if not self.stack:
+            self.stack.append((x, x))
+        else:
+            self.stack.append((x, min(x, self.stack[-1][1])))
 
     def pop(self):
         """
@@ -29,13 +65,15 @@ class MinStack(object):
         """
         :rtype: int
         """
-        return self.stack[-1]
+
+        return self.stack[-1][0]
 
     def getMin(self):
         """
         :rtype: int
         """
-        return min(self.stack)
+
+        return self.stack[-1][1]
 
 
 # Your MinStack object will be instantiated and called as such:
