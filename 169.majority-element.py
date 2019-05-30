de221841dfb96d739113a50d297ef09fbe1f3077
hjlarry <hjlarry@163.com>
2019-05-30 11:17:42 +0800

    add top voted solution of majority element

--- 169.majority-element.py
+++ 169.majority-element.py
@@ -7,15 +7,18 @@
 
 class Solution(object):
     # my solution 45%
-    from collections import Counter
+    # from collections import Counter
 
+    # def majorityElement(self, nums):
+    #     """
+    #     :type nums: List[int]
+    #     :rtype: int
+    #     """
+    #     items = Counter(nums)
+    #     return items.most_common(1)[0][0]
+    # top voted solution, 67%
     def majorityElement(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        items = Counter(nums)
-        return items.most_common(1)[0][0]
+        return sorted(nums)[len(nums) // 2]
 
 
 # print(Solution().majorityElement([3, 2, 3]))
