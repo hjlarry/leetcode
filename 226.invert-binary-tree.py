9996c6f84d6685f498cad5348b46bdfc6f5af6a3
hjlarry <hjlarry@163.com>
2019-06-10 19:18:48 +0800

    add invert binary tree top voted solution

--- 226.invert-binary-tree.py
+++ 226.invert-binary-tree.py
@@ -13,14 +13,34 @@
 
 class Solution(object):
     # my solution , 66%
+    # def invertTree(self, root):
+    #     """
+    #     :type root: TreeNode
+    #     :rtype: TreeNode
+    #     """
+    #     if root is not None:
+    #         root.left, root.right = root.right, root.left
+    #         self.invertTree(root.left)
+    #         self.invertTree(root.right)
+    #     return root
+
+    # top voted one line solution, 98%
+    # def invertTree(self, root):
+    #     if root:
+    #         root.left, root.right = (
+    #             self.invertTree(root.right),
+    #             self.invertTree(root.left),
+    #         )
+    #         return root
+
+    #  not use recusive solution, 92%
     def invertTree(self, root):
-        """
-        :type root: TreeNode
-        :rtype: TreeNode
-        """
-        if root is not None:
-            root.left, root.right = root.right, root.left
-            self.invertTree(root.left)
-            self.invertTree(root.right)
+        stack = [root]
+        while stack:
+            node = stack.pop()
+            if node:
+                node.left, node.right = node.right, node.left
+                stack.append(node.left)
+                stack.append(node.right)
         return root
 
