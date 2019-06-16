35049953f1d8b4c86ad93dc8bab47510a0c27d7c
hjlarry <hjlarry@163.com>
2019-06-16 15:31:38 +0800

    add preorder for leetcode

---
+++ 144.binary-tree-preorder-traversal.py
@@ -0,0 +1,26 @@
+#
+# @lc app=leetcode id=144 lang=python3
+#
+# [144] Binary Tree Preorder Traversal
+#
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    # my solution, 95%
+    def preorderTraversal(self, root: TreeNode) -> List[int]:
+        result = []
+        def helper(node, res):
+            if node:
+                res.append(node.val)
+                helper(node.left, res)
+                helper(node.right, res)
+        helper(root, result)
+        return result
+
+                    
+
