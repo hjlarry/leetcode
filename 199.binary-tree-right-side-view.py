016272094b7c006041ca9b4b5bacd04209b7bd56
hjlarry <hjlarry@163.com>
2019-06-17 11:49:23 +0800

    add btree right side view top voted solution

--- 199.binary-tree-right-side-view.py
+++ 199.binary-tree-right-side-view.py
@@ -13,20 +13,42 @@
 
 class Solution:
     # my solution use layer travel, 78.6%
-    def rightSideView(self, root: TreeNode) -> List[int]:
+    # def rightSideView(self, root: TreeNode) -> List[int]:
+    #     if not root:
+    #         return []
+    #     current_nodes = [root]
+    #     next_nodes = []
+    #     res = []
+    #     while current_nodes or next_nodes:
+    #         for node in current_nodes:
+    #             if node.left:
+    #                 next_nodes.append(node.left)
+    #             if node.right:
+    #                 next_nodes.append(node.right)
+    #         res.append(current_nodes[-1].val)
+    #         current_nodes = next_nodes
+    #         next_nodes = []
+    #     return res
+
+    # top voted solution , 78%, O(n)
+    # def rightSideView(self, root: TreeNode):
+    #     res = []
+
+    #     def collect(node, depth):
+    #         if node:
+    #             if depth == len(res):
+    #                 res.append(node.val)
+    #             collect(node.right, depth + 1)
+    #             collect(node.left, depth + 1)
+
+    #     collect(root, 0)
+    #     return res
+
+    # top voted solution , 91%, 但对于非常不平衡的树，可能是O(n^2)
+    def rightSideView(self, root: TreeNode):
         if not root:
             return []
-        current_nodes = [root]
-        next_nodes = []
-        res = []
-        while current_nodes or next_nodes:
-            for node in current_nodes:
-                if node.left:
-                    next_nodes.append(node.left)
-                if node.right:
-                    next_nodes.append(node.right)
-            res.append(current_nodes[-1].val)
-            current_nodes = next_nodes
-            next_nodes = []
-        return res
+        right = self.rightSideView(root.right)
+        left = self.rightSideView(root.left)
+        return [root.val] + right + left[len(right) :]
 
