ee67bb0f4eaee28cd9f956b5d89270cfe9ec0967
hjlarry <hjlarry@163.com>
2019-06-17 10:39:09 +0800

    add btree use stack inorder travel

---
+++ 94.binary-tree-inorder-traversal.py
@@ -0,0 +1,28 @@
+#
+# @lc app=leetcode id=94 lang=python3
+#
+# [94] Binary Tree Inorder Traversal
+#
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+
+class Solution:
+    # top voted use stack, 99%
+    def inorderTraversal(self, root: TreeNode) -> List[int]:
+        res = []
+        stack = []
+        while True:
+            while root:
+                stack.append(root)
+                root = root.left
+            if not stack:
+                return res
+            node = stack.pop()
+            res.append(node.val)
+            root = node.right
+
