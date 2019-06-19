a7ffd238174ce277827c062685e22c6de3aaa95b
hjlarry <hjlarry@163.com>
2019-06-19 14:15:17 +0800

    add longest valid parenth top voted solution

--- stack/32.longest-valid-parentheses.py
+++ stack/32.longest-valid-parentheses.py
@@ -4,6 +4,21 @@
 # [32] Longest Valid Parentheses
 #
 class Solution:
+    # top voted solution, 97%
     def longestValidParentheses(self, s: str) -> int:
-        
+        stack = [0] # 用最后一位存储最长的数值
+        longest = 0
+
+        for ch in s:
+            if ch == '(':
+                stack.append(0)
+            elif ch == ')':
+                if len(stack) >1:
+                    val = stack.pop()
+                    stack[-1] += val + 2
+                    longest = max(longest, stack[-1])
+                else:
+                    stack = [0]
+        return longest
+
 
