7a08b7ab3744a5be23779e0cbb706dec112449b7
he jiale <hjlarry@163.com>
2019-06-22 20:58:31 +0800

    add construct btree top voted solution

--- binarytree/889.construct-binary-tree-from-preorder-and-postorder-traversal.py
+++ binarytree/889.construct-binary-tree-from-preorder-and-postorder-traversal.py
@@ -12,5 +12,30 @@
 
 
 class Solution:
-    def constructFromPrePost(self, pre: List[int], post: List[int]) -> TreeNode:
-        pass
+    # top voted solution, 82%
+    # def constructFromPrePost(self, pre, post):
+    #     if not pre:
+    #         return None
+    #     root = TreeNode(pre[0])
+    #     if len(pre) == 1:
+    #         return root
+    #     l = post.index(pre[1])+1
+    #     root.left = self.constructFromPrePost(pre[1:l+1], post[:l])
+    #     root.right = self.constructFromPrePost(pre[l+1:], post[l:-1])
+    #     return root
+
+    # another solution use stack, 100%
+    def constructFromPrePost(self, pre, post):
+        stack = [TreeNode(pre[0])]
+        j = 0
+        for v in pre[1:]:
+            node = TreeNode(v)
+            while stack[-1].val == post[j]:
+                stack.pop()
+                j+=1
+            if not stack[-1].left:
+                stack[-1].left = node
+            else:
+                stack[-1].right = node
+            stack.append(node)
+        return stack[0]
