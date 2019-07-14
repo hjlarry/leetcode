d3ff39a35fa4fcff116e9750a1169d5251396ecf
he jiale <hjlarry@163.com>
2019-07-14 11:28:21 +0800

    add imp stack using queue top voted solution

--- stack/225.implement-stack-using-queues.py
+++ stack/225.implement-stack-using-queues.py
@@ -5,6 +5,54 @@
 #
 
 # my solution, 76%
+# from collections import deque
+# class MyStack(object):
+
+#     def __init__(self):
+#         """
+#         Initialize your data structure here.
+#         """
+#         self.master = deque()
+#         self.slave = deque()
+        
+
+#     def push(self, x):
+#         """
+#         Push element x onto stack.
+#         :type x: int
+#         :rtype: None
+#         """
+#         while len(self.master):
+#             self.slave.appendleft(self.master.pop())
+#         self.master.appendleft(x)
+#         while len(self.slave):
+#             self.master.appendleft(self.slave.pop())
+
+#     def pop(self):
+#         """
+#         Removes the element on top of the stack and returns that element.
+#         :rtype: int
+#         """
+#         return self.master.pop()
+        
+
+#     def top(self):
+#         """
+#         Get the top element.
+#         :rtype: int
+#         """
+#         return self.master[-1]
+        
+
+#     def empty(self):
+#         """
+#         Returns whether the stack is empty.
+#         :rtype: bool
+#         """
+#         return not len(self.master)
+        
+
+# top voted solution, 54%
 from collections import deque
 class MyStack(object):
 
@@ -12,9 +60,8 @@ class MyStack(object):
         """
         Initialize your data structure here.
         """
-        self.master = deque()
-        self.slave = deque()
-        
+        self.items = deque()
+
 
     def push(self, x):
         """
@@ -22,18 +69,16 @@ class MyStack(object):
         :type x: int
         :rtype: None
         """
-        while len(self.master):
-            self.slave.appendleft(self.master.pop())
-        self.master.appendleft(x)
-        while len(self.slave):
-            self.master.appendleft(self.slave.pop())
+        self.items.append(x)
+        for _ in range(len(self.items)-1):
+            self.items.append(self.items.popleft())
 
     def pop(self):
         """
         Removes the element on top of the stack and returns that element.
         :rtype: int
         """
-        return self.master.pop()
+        return self.items.popleft()
         
 
     def top(self):
@@ -41,7 +86,7 @@ class MyStack(object):
         Get the top element.
         :rtype: int
         """
-        return self.master[-1]
+        return self.items[0]
         
 
     def empty(self):
@@ -49,9 +94,8 @@ class MyStack(object):
         Returns whether the stack is empty.
         :rtype: bool
         """
-        return not len(self.master)
+        return not len(self.items)
         
-
 
 # Your MyStack object will be instantiated and called as such:
 # obj = MyStack()
